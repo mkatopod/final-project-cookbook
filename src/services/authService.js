4637a82f22e30fccdc671bc450fb61886a33c208
Maria <mkatopod@charlotte.edu>
2026-04-28 12:58:50 -0400

    Updated prisma:

--- src/services/authService.js
+++ src/services/authService.js
@@ -27,7 +27,7 @@ export async function logIn({ email, password }) {
     const token = jwt.sign({ 
         userId: user.user_id, role: user.role }, 
         process.env.JWT_SECRET, 
-        { expiresIn: '1h' }
+        { expiresIn: "1hr" }
     );
     
     return { 
